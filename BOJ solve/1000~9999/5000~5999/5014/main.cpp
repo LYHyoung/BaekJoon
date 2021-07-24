92a405b2061da8a7a81d0ec67a12fd4ec4f6e96a
YH Lee <lyh4186@naver.com>
2021-07-24 21:43:55 +0900

    Add files via upload

--- BOJ solve/1000~9999/5000~5999/5014/main.cpp
+++ BOJ solve/1000~9999/5000~5999/5014/main.cpp
@@ -1,39 +1,42 @@
 #include <iostream>
 #include <queue>
+
 #define MAX 1000001
 using namespace std;
+
 int visited[MAX];
 int F, S, G, U, D;
-void bfs() {
-	queue<int> q;
-	q.push(S);
+
+int bfs() {
+	queue<pair<int, int>> q;
+	q.push(make_pair(S, 0));
 	visited[S] = 1;
+
 	while (!q.empty()) {
-		int cur = q.front();
+		int cur = q.front().first;
+		int cnt = q.front().second;
 		q.pop();
-		if (cur == G)			// µµÂø ¿Ï·á
-			break;
-		if (cur + U > F)		// FÃþ À§
-			continue;
-		else if (visited[cur + U] == 0) {
-			q.push(cur + U);
-			visited[cur + U] = visited[cur] + 1;
+
+		if (cur == G) return cnt;
+
+		if (cur + U <= F && visited[cur + U] == 0) {
+			q.push(make_pair(cur + U, cnt + 1));
+			visited[cur + U] = 1;
 		}
-		if (cur - D < 1)		// 1Ãþ ¾Æ·¡
-			continue;
-		else if (visited[cur - D] == 0) {
-			visited[cur - D] = visited[cur] + 1;
-			q.push(cur - D);
+		if (cur - D > 0 && visited[cur - D] == 0) {
+			visited[cur - D] = 1;
+			q.push(make_pair(cur - D, cnt + 1));
 		}
 	}
+	return -1;
 }
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> F >> S >> G >> U >> D;
-	bfs();
-	if (visited[G] == 0)
+	int ans = bfs();
+	if (ans == -1)
 		cout << "use the stairs" << '\n';
 	else 
-		cout << visited[G] - 1 << '\n';
+		cout << ans << '\n';
 	return 0;
 }
